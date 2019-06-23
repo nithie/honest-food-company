a7e0bbb8b3c5635f0fb2ec8895693470bfbf8efe
Nithyaprasanth Pasupathy <nithyaprasanth@simplilearn.net>
2019-06-23 20:09:24 +0530

    added missed files

--- server/app.js
+++ server/app.js
@@ -9,13 +9,13 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public', 'client')));
 app.use(function(req, res, next) {
     if (!req.headers.authorization || (req.headers.authorization && req.headers.authorization != '$n1th1e@123')) {
         return res.status(403).json({error: 'No credentials sent!'})
     }
     next();
 })
-app.use(express.static(path.join(__dirname, 'public', 'client')));
 
 app.use('/api/v1', indexRouter);
 
